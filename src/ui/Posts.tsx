e28d3c9de262242a6c4a9dba868b0e644a0a23bd
Alexander Kachkaev <alexander@kachkaev.ru>
2020-08-18 22:03:01 +0100

    Minimal fix in Posts state

--- src/ui/Posts.tsx
+++ src/ui/Posts.tsx
@@ -1,44 +1,52 @@
 import React from "react";
-import { useState, useEffect } from 'react';
-import {Post} from "./Post"
+
+import { Post } from "./Post";
 
 interface PostData {
-  date:number,
-  header:string,
-  content:string
-}
-interface PostsInterface {
-  postType:number
+  date: number;
+  header: string;
+  content: string;
 }
 
-const fetchSinglePost = async (postUrl: string):Promise<PostData> => {
+const fetchSinglePost = async (postUrl: string): Promise<PostData> => {
   const response = await fetch(postUrl);
   return (await response.json()) as PostData;
 };
-const getListOfPosts = async(): Promise<any> =>
-await fetchSinglePost('posts.json');
 
-const loadPosts = async (postUrls:string[]): Promise<any> => 
-await Promise.all(postUrls.map((postUrl) => fetchSinglePost(postUrl)));
+const getListOfPosts = async (): Promise<any> =>
+  await fetchSinglePost("posts.json");
 
-const Posts: any = (props:PostsInterface) => {
-    const [postUrls,setURLs] = useState([]);
-    const [posts, setPosts] = useState([]);
-    useEffect(() => {
-      getListOfPosts().then((list)=> setURLs(list));
-      loadPosts(postUrls).then((posts) => setPosts(posts));
-    }, []);
-  
-    return (
-      <div className="App">
-        {posts.map((post, index) => {
-          //if(Math.random()>0.51) console.log(post);
-          //FIXME:
-          const n = Math.round(Math.random()*5)
-          //import(`./1234${n}`)
-          return <Post key={index} data={post} />
+const loadPosts = async (postUrls: string[]): Promise<any> =>
+  await Promise.all(postUrls.map((postUrl) => fetchSinglePost(postUrl)));
+
+interface PostsProps {
+  postType: number;
+}
+
+const Posts: React.FunctionComponent<PostsProps> = () => {
+  const [postUrls, setPostUrls] = React.useState([]);
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    getListOfPosts().then((list) => setPostUrls(list));
+  }, []);
+
+  React.useEffect(() => {
+    if (postUrls.length) {
+      loadPosts(postUrls).then((loadedPosts) => setPosts(loadedPosts));
+    }
+  }, [postUrls]);
+
+  return (
+    <div className="App">
+      {posts.map((post, index) => {
+        //if(Math.random()>0.51) console.log(post);
+        // FIXME:
+        const n = Math.round(Math.random() * 5) + 1;
+        //import(`./1234${n}`)
+        return <Post key={index} data={post} />;
       })}
-      </div>
-    );
+    </div>
+  );
 };
 export default Posts;
