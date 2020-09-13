d0cafc7af73ad1b420600614157d4dd5fba17aba
marat <nagaevmt49@gmail.com>
2020-09-13 11:35:54 +0300

    Add mobile detection

--- src/ui/Menu.tsx
+++ src/ui/Menu.tsx
@@ -1,34 +1,61 @@
-import React from "react"
-import {LogModal,RegModal} from "./Modal"
-import {getDefaultTheme} from "./utils";
+import React from "react";
 
-type Function = (n:number) => any;
+import { LogModal, RegModal } from "./Modal";
+import { detectMobile, getDefaultTheme } from "./utils";
 
-interface MenuInterface{
-    updateState:Function
+type Function = (n: number) => any;
+
+interface MenuInterface {
+  updateState: Function;
 }
-
-function Menu(props:MenuInterface){
-    const style = getDefaultTheme();
-    const {updateState} = props;
-    const [isLogOpen,setLogOpen] = React.useState(false);
-    const [isRegOpen,setRegOpen] = React.useState(false);
-    const showLogIn = () => setLogOpen(true);
-    const showRegistration = () => setRegOpen(true);
-    return (
-        <div id="menu">
-            <div>
-                <a style={style.log} onClick={showLogIn}>Вход</a>&nbsp;
-                <a style={style.reg} onClick={showRegistration}>Регистрация</a>&nbsp;
-            </div>
-            <RegModal isOpen={isRegOpen} setIsOpen={setRegOpen} />
-            <LogModal isOpen={isLogOpen} setIsOpen={setLogOpen} />
-            <div>
-                <a onClick={()=>updateState(0)}>Блог редакции</a>&nbsp;
-                <a onClick={()=>updateState(1)}>Больницы</a>&nbsp;
-                <a onClick={()=>updateState(2)}>Заводы</a>
-            </div>
-        </div>
-    );
+function DesktopMenu(props: any) {
+  return (
+    <div>
+      <a onClick={() => props.updateState(0)}>Блог редакции</a>&nbsp;
+      <a onClick={() => props.updateState(1)}>Больницы</a>&nbsp;
+      <a onClick={() => props.updateState(2)}>Заводы</a>
+    </div>
+  );
+}
+function MobileMenu(props: any) {
+  const c = (event) => props.updateState(+event.target.value);
+  return (
+    <select onChange={c}>
+      <option value="0">Блог редакции</option>
+      <option value="1">Больницы</option>
+      <option value="2">Заводы</option>
+    </select>
+  );
+}
+function Menu(props: MenuInterface) {
+  const style = getDefaultTheme();
+  const { updateState } = props;
+  const [isLogOpen, setLogOpen] = React.useState(false);
+  const [isRegOpen, setRegOpen] = React.useState(false);
+  const showLogIn = () => setLogOpen(true);
+  const showRegistration = () => setRegOpen(true);
+  const isMobile = detectMobile();
+  const menu = isMobile ? (
+    <MobileMenu updateState={updateState} />
+  ) : (
+    <DesktopMenu updateState={updateState} />
+  );
+  return (
+    <div id="menu">
+      <div>
+        <a style={style.log} onClick={showLogIn}>
+          Вход
+        </a>
+        &nbsp;
+        <a style={style.reg} onClick={showRegistration}>
+          Регистрация
+        </a>
+        &nbsp;
+      </div>
+      <RegModal isOpen={isRegOpen} setIsOpen={setRegOpen} />
+      <LogModal isOpen={isLogOpen} setIsOpen={setLogOpen} />
+      {menu}
+    </div>
+  );
 }
 export default Menu;
--- src/ui/utils.ts
+++ src/ui/utils.ts
@@ -1,10 +1,17 @@
 import dark from "./dark";
-function wrap(f:Function,...args){
-    return ()=>{
-        f.apply(undefined,args)
-    }
+function wrap(f: Function, ...args) {
+  return () => {
+    // eslint-disable-next-line prefer-spread
+    f.apply(undefined, args);
+  };
 }
-function getDefaultTheme(){
-    return dark;
+function getDefaultTheme() {
+  return dark;
 }
-export {wrap,getDefaultTheme};
+function detectMobile() {
+  const ua = navigator.userAgent;
+  const isAndroid = ua.match(/Android/i);
+  const isIOS = ua.match(/iPhone|iPad|iPod/i);
+  return isAndroid || isIOS;
+}
+export { wrap, getDefaultTheme, detectMobile };
